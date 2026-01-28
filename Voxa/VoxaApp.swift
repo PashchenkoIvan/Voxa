9a91bb426921dd8e18a7888a0412499c9917bb27
IvanPashchenko <vanapasenko886@gmail.com>
2026-01-28 04:34:11 +0100

    Initial Commit

---
+++ Voxa/VoxaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoxaApp.swift
+//  Voxa
+//
+//  Created by Пащенко Иван on 28.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct VoxaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
